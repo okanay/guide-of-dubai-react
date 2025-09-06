86ade8f185677a5bfea1290aff37d7c1f7ba83b6
Okan Ay <okanay@hotmail.com>
2025-09-06 16:42:18 +0300

    SSR Fix

--- src/hooks/use-click-outside.ts
+++ src/hooks/use-click-outside.ts
@@ -1,42 +1,42 @@
-import { useEffect, useRef, RefObject } from "react";
+import { useEffect, useRef, RefObject } from 'react'
 
-type Handler = (event: MouseEvent | TouchEvent) => void;
+type Handler = (event: MouseEvent | TouchEvent) => void
 
 function useClickOutside<T extends HTMLElement = HTMLElement>(
   handler: Handler,
   active: boolean = true,
   exceptRef?: RefObject<HTMLElement | null>,
 ): RefObject<T | null> {
-  const ref = useRef<T>(null);
+  const ref = useRef<T>(null)
 
   useEffect(() => {
-    if (!active) return;
+    if (!active || typeof document === 'undefined') return
 
     const listener = (event: MouseEvent | TouchEvent) => {
-      const el = ref.current;
-      const exceptEl = exceptRef?.current;
+      const el = ref.current
+      const exceptEl = exceptRef?.current
 
       if (
         !el ||
         el.contains(event.target as Node) ||
         (exceptEl && exceptEl.contains(event.target as Node))
       ) {
-        return;
+        return
       }
 
-      handler(event);
-    };
+      handler(event)
+    }
 
-    document.addEventListener("mousedown", listener);
-    document.addEventListener("touchstart", listener);
+    document.addEventListener('mousedown', listener)
+    document.addEventListener('touchstart', listener)
 
     return () => {
-      document.removeEventListener("mousedown", listener);
-      document.removeEventListener("touchstart", listener);
-    };
-  }, [handler, active, exceptRef]);
+      document.removeEventListener('mousedown', listener)
+      document.removeEventListener('touchstart', listener)
+    }
+  }, [handler, active, exceptRef])
 
-  return ref;
+  return ref
 }
 
-export default useClickOutside;
+export default useClickOutside
