8ba8383d8c4de9c05cf070c731cc6a5f111433dc
Okan Ay <okanay@hotmail.com>
2025-09-12 19:50:04 +0300

    continue to pages

--- src/routes/$lang/_public/activities.search.tsx
+++ src/routes/$lang/_public/activities.search.tsx
@@ -2,5 +2,6 @@ import { ActivitySearchPage } from '@/features/public/pages/activity/page-search
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/$lang/_public/activities/search')({
+  beforeLoad: async ({ search: { date, child, adult } }) => {},
   component: ActivitySearchPage,
 })
