024c30537687c5ed5fc47e3da6a53ad2d99f560b
Okan Ay <okanay@hotmail.com>
2025-09-16 11:03:58 +0300

    continue to pages

--- src/features/public/pages/hotel/layout.tsx
+++ src/features/public/pages/hotel/layout.tsx
@@ -1,14 +1,18 @@
 import { Route } from 'src/routes/$lang/_public/hotels.route'
 import { SearchForm } from './form-search'
 import { Outlet } from '@tanstack/react-router'
+import { PublicFooter } from '../../layout/footer'
 
 export const HotelLayout = () => {
   const search = Route.useSearch()
 
   return (
-    <main className="flex flex-col">
-      <SearchForm initialData={search} />
-      <Outlet />
-    </main>
+    <>
+      <main className="flex flex-col">
+        <SearchForm initialData={search} />
+        <Outlet />
+      </main>
+      <PublicFooter />
+    </>
   )
 }
